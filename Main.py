faa769e4d847432d00296791a4ace025fea5331c
William Jagels <will.jagels@gmail.com>
2015-01-30 20:38:09 -0500

    Re time...

--- Main.py
+++ Main.py
@@ -31,36 +31,36 @@ __book_data2__ = {
 }
 
 
-from pymongo import MongoClient
 import urllib3
 import json
 import http.cookies
 import re
-import pprint
 import requests
-from time import sleep
+from pymongo import MongoClient
 
 
 headers = {'content-type': 'application/json'}
-conn = urllib3.PoolManager()
+conn = urllib3.PoolManager() # TODO: migrate to requests
+client = MongoClient()
+db = client.test
 
 
 def main():
     # Have to get cookies so we look legit
-    r = conn.urlopen('GET', __base_url__)
+    r = conn.urlopen('GET', __base_url__) # TODO: migrate to requests
     print('HDR:', r.headers)
     print('DATA:', r.data)
     cookie = http.cookies.SimpleCookie(r.headers['set-cookie'])
     headers['Cookie'] = cookie.output(attrs=[], header='').strip()
     headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.35 Safari/537.36'
-    r = conn.urlopen('GET', __req_url__, headers=headers)
+    r = conn.urlopen('GET', __req_url__, headers=headers) # TODO: migrate to requests
     print('DATA:', r.data)
     for dept in json.loads(r.data.decode("utf-8")):
         scrape_dept(dept)
 
 
 def renewCookie():
-    r = conn.urlopen('GET', __base_url__)
+    r = conn.urlopen('GET', __base_url__) # TODO: migrate to requests
     cookie = http.cookies.SimpleCookie(r.headers['set-cookie'])
     headers['Cookie'] = cookie.output(attrs=[], header='').strip()
 
@@ -68,12 +68,12 @@ def renewCookie():
 def scrape_dept(dept):
     renewCookie() # Do this periodically so we don't get rate limited
     url = __dept_url__ + '&deptId=' + dept['categoryId']
-    r = conn.urlopen('GET', url, headers=headers)
+    r = conn.urlopen('GET', url, headers=headers) # TODO: migrate to requests
     print("New dept!", dept['categoryName'])
     try:
         data = json.loads(r.data.decode("utf-8"))
         for cl in data:
-            scrape_class(cl)
+            scrape_class(cl, dept)
     except:
         print(headers)
         print(r.data)
@@ -83,45 +83,43 @@ def scrape_dept(dept):
         pass
 
 
-def scrape_class(cl):
+def scrape_class(cl, dept):
     url = __class_url__ + '&courseId=' + cl['categoryId']
-    r = conn.urlopen('GET', url, headers=headers)
+    r = conn.urlopen('GET', url, headers=headers) # TODO: migrate to requests
     try:
         data = json.loads(r.data.decode("utf-8"))
         for section in data:
-            scrape_section(section)
+            scrape_section(section, cl, dept)
     except:
         print(r.data)
         print("Shit, something bad happened")
         renewCookie()
-        scrape_class(cl)
+        scrape_class(cl, dept)
         pass
 
 
-def scrape_section(section):
+def scrape_section(section, cl, dept):
     url = __book_url__
     urldata = __book_data2__
     urldata.update({'section_1': section['categoryId']})
     myheaders = headers.copy()
     myheaders['content-type'] = 'application/x-www-form-urlencoded'
-    #urldata = urllib3.encode_multipart_formdata(urldata)
-    #r = conn.request('POST', url, body=urldata, headers=headers)
     try:
         r = requests.post(url, data=urldata, headers=myheaders, timeout=10)
-        #print(r.text)
-        # print(grep(r.data.decode("utf-8"), "bookPrice"))
-        #print(grep(r.text, "bookPrice"))
+        extractPrices(r.text, section, cl, dept)
     except:
         print("Oh noes!")
         renewCookie()
-        scrape_section(section)
+        scrape_section(section, cl, dept)
         pass
 
 
-def extractPrices():
-    return # TODO: make this dump stuff into mongodb
+def extractPrices(html, section, cl, dept):
+    print(grep(html, "bookPrice"))
+    print(re.finditer("span class=\"bookPrice\" title=\"(.*?)\"", html))
 
-def grep(s,pattern):
+
+def grep(s, pattern):
     return '\n'.join(re.findall(r'^.*%s.*?$'%pattern,s,flags=re.M))
 
 
